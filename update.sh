a6951f0d00de6f95aa769436fbc67d4bf44ba335
Karl Ljungkvist <k.ljungkvist@gmail.com>
2013-08-28 18:58:09 +0200

    generalisation of update script

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@
 # @author Karl Ljungkvist <karl.ljungkvist.0193@student.uu.se>
 
 
-LOCALBRANCH=`git branch | grep local | sed 's/.* \([a-z0-9]*-local\)/\1/'`
+LOCALBRANCH="$(hostname)-local"
 
 git stash && git checkout master && git pull && git checkout $LOCALBRANCH && git rebase master && git stash pop
