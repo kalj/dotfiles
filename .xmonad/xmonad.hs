efe1ee142a1b46ec5382358764b1e73f1250c99a
Karl Ljungkvist <k.ljungkvist@gmail.com>
2017-02-09 10:32:26 +0100

    [xmonad] use xmonad-log-applet

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -27,15 +27,18 @@
 --     }
 
 import XMonad
-import XMonad.Util.EZConfig
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.SetWMName
-import XMonad.Hooks.ICCCMFocus
+import qualified XMonad.StackSet as S
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Hooks.EwmhDesktops (ewmhDesktopsStartup)
+import XMonad.Hooks.SetWMName (setWMName)
+import XMonad.Hooks.ICCCMFocus (takeTopFocus)
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+
 import Data.Map (fromList, toList)
-import Data.Maybe (mapMaybe)
+import Data.Maybe (isJust, catMaybes, mapMaybe, fromMaybe, listToMaybe)
+import Data.List (intersperse)
 
 import XMonad.Config.Mate
 import XMonad.Layout.NoBorders
@@ -43,6 +46,15 @@ import XMonad.Layout.PerWorkspace
 import XMonad.Layout.IM
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Reflect
+
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
+import Codec.Binary.UTF8.String (encodeString)
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.NamedWindows
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.WorkspaceCompare (WorkspaceSort)
 
 import MaxWidth
 
@@ -124,20 +136,109 @@ myManageHook = composeAll
 -- =============================================================================
 -- Log hook
 -- =============================================================================
+getWellKnownName :: D.Client -> IO ()
+getWellKnownName dbus = do
+  D.requestName dbus (D.busName_ "org.xmonad.Log")
+                [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+  return ()
+
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal (D.objectPath_ "/org/xmonad/Log") (D.interfaceName_ "org.xmonad.Log") (D.memberName_ "Update")) {
+--             D.signalBody = [D.toVariant ("<b>" ++ (UTF8.decodeString str) ++ "</b>")]
+            D.signalBody = [D.toVariant ((UTF8.decodeString str))]
+        }
+    D.emit dbus signal
+
+--
+-- iconsAppletLog based on dynamicLogWithPP
+-- (in addition to workspace numbers, it also outputs window id-s)
+--
+
+iconsAppletLog :: D.Client -> X ()
+iconsAppletLog dbus = iconsAppletLogString >>= io . (dbusOutput dbus)
+
+iconsAppletLogString :: X String
+iconsAppletLogString = do
+
+    winset <- gets windowset
+    urgents <- readUrgents
+    sort' <- ppSort iconsAppletPP
+
+    -- layout description
+    let ld = description . S.layout . S.workspace . S.current $ winset
+
+    -- workspace list
+    let ws = iconsAppletWindowSet sort' urgents winset
+
+    -- window title
+    wt <- maybe (return "") (fmap show . getName) . S.peek $ winset
+
+    -- run extra loggers, ignoring any that generate errors.
+    extras <- mapM (flip catchX (return Nothing)) $ ppExtras iconsAppletPP
+
+    return $ encodeString . sepBy (ppSep iconsAppletPP) . ppOrder iconsAppletPP $
+                        [ ws
+                        , ppLayout iconsAppletPP ld
+                        , ppTitle iconsAppletPP wt
+                        ]
+                        ++ catMaybes extras
+
+iconsAppletWindowSet :: WorkspaceSort -> [Window] -> WindowSet -> String
+iconsAppletWindowSet sort' urgents s = sepBy ";" . map fmt . sort' $
+            map S.workspace (S.current s : S.visible s) ++ S.hidden s
+   where this     = S.currentTag s
+         visibles = map (S.tag . S.workspace) (S.visible s)
+
+         fmt w = printer iconsAppletPP (iconsAppletFormatWorkspace w)
+          where printer | any (\x -> maybe False (== S.tag w) (S.findTag x s)) urgents  = ppUrgent
+                        | S.tag w == this                                               = ppCurrent
+                        | S.tag w `elem` visibles                                       = ppVisible
+                        | isJust (S.stack w)                                            = ppHidden
+                        | otherwise                                                     = ppHiddenNoWindows
+
+iconsAppletFormatWorkspace :: S.Workspace WorkspaceId (Layout Window) Window -> String
+iconsAppletFormatWorkspace w = (S.tag w) ++ ":" ++ (iconsAppletFormatStack . S.stack $ w)
+
+iconsAppletFormatStack :: Maybe (S.Stack Window) -> String
+iconsAppletFormatStack s = case s of
+  Just value -> sepBy "," . map show $ (reverse (S.up value) ++ [S.focus value] ++ S.down value)
+  Nothing -> ""
+
+iconsAppletPP :: PP
+iconsAppletPP = defaultPP {
+    ppTitle    = ("|" ++)
+  , ppCurrent  = ("*" ++)
+  , ppVisible  = ("+" ++)
+  , ppHidden   = ("-" ++)
+  , ppUrgent   = ("!" ++)
+  , ppLayout   = const ""
+  , ppSep      = ";"
+  }
+
+sepBy :: String   -- ^ separator
+      -> [String] -- ^ fields to output
+      -> String
+sepBy sep = concat . intersperse sep . filter (not . null)
 
 
 -- =============================================================================
 -- Main
 -- =============================================================================
 
-main = do
-  xmonad $ mateConfig {
-         modMask            = myModMask
-       , manageHook = myManageHook <+> manageHook mateConfig
-       , layoutHook = myLayoutHook
-       , logHook = logHook mateConfig >> takeTopFocus
-       , startupHook = startupHook mateConfig >> setWMName "LG3D"
-       , XMonad.keys = myAddKPs  . (XMonad.keys mateConfig)
-    } `additionalKeysP` myKeys
+myConfig dbus = mateConfig {
+  modMask    = myModMask
+  , manageHook = myManageHook <+> manageHook mateConfig
+  , layoutHook = myLayoutHook
+  , logHook = logHook mateConfig >> takeTopFocus >> iconsAppletLog dbus
+  , startupHook = startupHook mateConfig >> setWMName "LG3D"
+  , XMonad.keys = myAddKPs  . (XMonad.keys mateConfig)
+  } `additionalKeysP` myKeys
 
 -- . (Data.Map.union (mkKeymap gnomeConfig myKeys))
+
+main :: IO ()
+main = do
+  dbus <- D.connectSession
+  getWellKnownName dbus
+  xmonad $ myConfig dbus
